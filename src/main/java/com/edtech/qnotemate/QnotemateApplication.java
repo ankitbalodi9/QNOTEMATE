a59ae35e73dde6ffd87af636e9121c1e0fc0b0bd
abalodi <abalodi@chegg.com>
2023-07-15 14:17:44 +0530

    intial_commit

---
+++ src/main/java/com/edtech/qnotemate/QnotemateApplication.java
@@ -0,0 +1,13 @@
+package com.edtech.qnotemate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QnotemateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QnotemateApplication.class, args);
+	}
+
+}
